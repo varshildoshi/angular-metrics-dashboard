f05361c23bd8f5ca2435d648dcbbfc3d3d340846
varshildoshi <varshil.mscit16@gmail.com>
2025-05-08 11:28:46 +0530

    delete dist folder

--- src/app/modules/services/data.service.ts
+++ src/app/modules/services/data.service.ts
@@ -1,15 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import dataFile from '../../../assets/mock-metrics.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  private urlDataFile = 'assets/mock-metrics.json';
+
   constructor(private httpClient: HttpClient) { }
 
   getDashboardDetails() {
-    return this.httpClient.get(`${environment.baseURL}`);
+    return this.httpClient.get(this.urlDataFile);
   }
 }
